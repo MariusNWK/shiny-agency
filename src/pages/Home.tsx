fb45a78100ca068664c096d6edff44c191ac4013
MariusNWK <marius.nowak@epitech.eu>
2022-09-23 17:09:53 +0200

    init repo

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import "../styles/Home.css";
+
+function Home() {
+  return (
+    <div>
+      <h1>Nouveau cours</h1>
+      <div>React - notions avancées</div>
+    </div>
+  );
+}
+
+export default Home;
